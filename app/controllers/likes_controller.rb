2a8e4e3bd03a3352cdfcef8c4978992a7e5808c4
DavidLineman <dline215@gmail.com>
2022-09-14 12:53:20 -0400

    added likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,4 +4,9 @@ class LikesController < ApplicationController
     current_user.like!
   end 
 
+  def destroy
+    @post = current_user.posts.find_by(id: params[:post_id])
+    current_user.likes.where(post_id: @post.id).delete_all
+  end 
+
 end
